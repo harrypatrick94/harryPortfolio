bce9880298b46db740d8cd3fe99375b34cac12b0
Harry Patrick <harry.g.patirck94@gmail.com>
2020-04-14 08:27:02 +1000

    depoly

--- src/index.js
+++ src/index.js
@@ -10,3 +10,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// http://harrypatrick.com.s3-website-ap-southeast-2.amazonaws.com
